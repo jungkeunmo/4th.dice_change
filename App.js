762bae202f5ba7a10ec991c475b3b1450a0ac48a
jungkeunmo <abc59684495@gmail.com>
2023-04-02 10:20:46 +0900

    dice

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { React, useCallback, useState } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ImageBackground,
+} from "react-native";
 
-export default function App() {
+const App = () => {
+  const [cn, setCn] = useState(0);
+  const color = [
+    "homework7/assets/images/img1.png",
+    "homework7/assets/images/img2.png",
+    "homework7/assets/images/img3.png",
+    "homework7/assets/images/img4.png",
+    "homework7/assets/images/img5.png",
+    "homework7/assets/images/img6.png",
+  ];
+
+  const checkHandler = () => {
+    const ran = Math.floor(Math.random() * 6);
+    if (ran === cn) {
+      checkHandler();
+    } else {
+      return setCn(ran);
+    }
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        style={{
+          width: 150,
+          height: 150,
+          margin: 10,
+          borderRadius: 8,
+        }}
+        source={requir(cn)}
+      ></ImageBackground>
+      <View>
+        <Button
+          style={{
+            color: "#000",
+          }}
+          title="버튼"
+          onPress={() => checkHandler()}
+        />
+      </View>
+    </SafeAreaView>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
